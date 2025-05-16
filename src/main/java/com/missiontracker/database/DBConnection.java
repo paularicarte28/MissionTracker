b440ef93592a8e79e832f6d8c9590af425db7482
P.Caam <pcaamano@outlook.es>
2025-05-16 10:39:23 +0200

    Configuración de la base de datos

---
+++ src/main/java/com/missiontracker/database/DBConnection.java
@@ -0,0 +1,22 @@
+package com.missiontracker.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/missiontracker";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Admin";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database connection failed");
+        
+        }
+    }
+}
