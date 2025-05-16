246d96682c6d6f0cb7254033062c243f1fa0d35a
paularicarte28 <a28729@svalero.com>
2025-05-16 12:31:33 +0200

    creacion de fet list de astronautas y modificaciones en models

---
+++ src/main/java/com/missiontracker/dao/AstronautDAO.java
@@ -0,0 +1,40 @@
+package com.missiontracker.dao;
+
+import com.missiontracker.model.Astronaut;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AstronautDAO {
+
+    private Connection connection;
+
+    public AstronautDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<Astronaut> getAllAstronauts() {
+        List<Astronaut> astronauts = new ArrayList<>();
+        try {
+            String sql = "SELECT a.*, m.name AS mission_name " +
+                    "FROM astronaut a LEFT JOIN mission m ON a.mission_id = m.id";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                Astronaut a = new Astronaut(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("nationality"),
+                        rs.getString("role"),
+                        rs.getInt("mission_id"));
+                a.setMissionName(rs.getString("mission_name"));
+                astronauts.add(a);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return astronauts;
+    }
+}
