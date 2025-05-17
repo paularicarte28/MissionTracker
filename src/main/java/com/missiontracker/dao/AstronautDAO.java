b089dd593d5793c450b50164f3ca16c37850c79c
paularicarte28 <a28729@svalero.com>
2025-05-17 10:37:53 +0200

    add data in astronuat id

--- src/main/java/com/missiontracker/dao/AstronautDAO.java
+++ src/main/java/com/missiontracker/dao/AstronautDAO.java
@@ -12,9 +12,9 @@ public class AstronautDAO {
     public AstronautDAO(Connection connection) {
         this.connection = connection;
     }
-
+    //GET BY ID
     public Astronaut getAstronautById(int id) {
-        Astronaut astronaut = null;
+    Astronaut astronaut = null;
         try {
             String sql = "SELECT a.*, m.name AS mission_name " +
                     "FROM astronauts a " +
@@ -29,7 +29,9 @@ public class AstronautDAO {
                 astronaut.setId(rs.getInt("id"));
                 astronaut.setName(rs.getString("name"));
                 astronaut.setNationality(rs.getString("nationality"));
-                // astronaut.setMissionName(rs.getString("mission_name"));
+                astronaut.setRole(rs.getString("role"));
+                astronaut.setMissionid(rs.getInt("mission_id"));
+                astronaut.setMissionName(rs.getString("mission_name")); // <-- ESTA LÍNEA ES CLAVE
             }
 
             stmt.close();
@@ -39,10 +41,51 @@ public class AstronautDAO {
         }
         return astronaut;
     }
+    //GET LIST
+    public List<Astronaut> getAllAstronauts() {
+        List<Astronaut> astronauts = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM astronauts ";
 
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Astronaut astronaut = new Astronaut();
+                astronaut.setId(rs.getInt("id"));
+                astronaut.setName(rs.getString("name"));
+                astronaut.setNationality(rs.getString("nationality"));
+                astronaut.setRole(rs.getString("role"));
+                astronauts.add(astronaut);
+            }
+            System.out.println("Astronauts encontrados: " + astronauts.size());
+
+            stmt.close();
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return astronauts;
+    }
+
+    // INSERT
+    public void insertAstronaut(Astronaut astronaut) {
+        String sql = "INSERT INTO astronauts (name, nationality, role, mission_id) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, astronaut.getName());
+            stmt.setString(2, astronaut.getNationality());
+            stmt.setString(3, astronaut.getRole());
+            stmt.setInt(4, astronaut.getMissionid());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //DELETE
       public Astronaut deleteAstronautById(int id) {
-    Astronaut astronaut = null;
-    try {
+        Astronaut astronaut = null;
+        try {
         String selectSql = "SELECT * FROM astronauts WHERE id = ?";
         PreparedStatement selectStmt = connection.prepareStatement(selectSql);
         selectStmt.setInt(1, id);
@@ -71,50 +114,8 @@ public class AstronautDAO {
     } catch (SQLException e) {
         e.printStackTrace();
     }
-    return astronaut;
-}
-
-    
-
-    public List<Astronaut> getAllAstronauts() {
-        List<Astronaut> astronauts = new ArrayList<>();
-        try {
-            String sql = "SELECT * FROM astronauts ";
-
-            PreparedStatement stmt = connection.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Astronaut astronaut = new Astronaut();
-                astronaut.setId(rs.getInt("id"));
-                astronaut.setName(rs.getString("name"));
-                astronaut.setNationality(rs.getString("nationality"));
-                astronaut.setRole(rs.getString("role"));
-                astronauts.add(astronaut);
-            }
-            System.out.println("Astronauts encontrados: " + astronauts.size());
-
-            stmt.close();
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return astronauts;
-        
+        return astronaut;
     }
 
-    // INSERT
-    public void insertAstronaut(Astronaut astronaut) {
-        String sql = "INSERT INTO astronauts (name, nationality, role, mission_id) VALUES (?, ?, ?, ?)";
-
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, astronaut.getName());
-            stmt.setString(2, astronaut.getNationality());
-            stmt.setString(3, astronaut.getRole());
-            stmt.setInt(4, astronaut.getMissionid());
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+ 
 }
